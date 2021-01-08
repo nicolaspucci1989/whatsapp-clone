7c8bf24bd75af3d2ba313277cd215d5417b53d14
Nicolas <nicolaspucci1989@gmail.com>
2021-01-08 19:53:37 -0300

    adding add new chat template

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -2,13 +2,19 @@ import React, { useEffect, useState } from 'react';
 import './SidebarChat.css'
 import { Avatar } from "@material-ui/core";
 
-const SidebarChat = () => {
+const SidebarChat = ({ addNewChat }) => {
   const [seed, setSeed] = useState('')
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000))
   }, [])
 
-  return (
+  const createChat = () => {
+    const roomname = prompt("Please enter the name of the chat")
+
+    if(roomname) {}
+  }
+
+  return !addNewChat ? (
     <div className="sidebarChat">
       <Avatar src={`http://avatars.dicebear.com/api/human/${seed}.svg`}/>
       <div className="sidebarChat__info">
@@ -16,7 +22,11 @@ const SidebarChat = () => {
         <p>Last message...</p>
       </div>
     </div>
-  );
+  ) : (
+    <div onClick={createChat} className="sidebarChat">
+      <h2>Add New Chat</h2>
+    </div>
+  )
 };
 
 export default SidebarChat;
