6efa2e99586b1b496690a09ad48bba661958e959
Nicolas <nicolaspucci1989@gmail.com>
2021-01-09 02:36:59 -0300

    adding login screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import './App.css';
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import Login from "./Login";
 
 function App() {
+  const [user, setUser] = useState(null)
+
   return (
     <div className="app">
-      <div className="app__body">
-        <Router>
-          <Sidebar/>
-          <Switch>
-            <Route path="/rooms/:roomId">
-              <Chat/>
-            </Route>
-            <Route path="/">
-              <h1>home</h1>
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+      {!user ? (
+          <Login/>
+        ) :
+        (
+          <div className="app__body">
+            <Router>
+              <Sidebar/>
+              <Switch>
+                <Route path="/rooms/:roomId">
+                  <Chat/>
+                </Route>
+                <Route path="/">
+                  <h1>home</h1>
+                </Route>
+              </Switch>
+            </Router>
+          </div>
+        )
+      }
     </div>
   );
 }
